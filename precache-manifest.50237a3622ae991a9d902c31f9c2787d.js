bc535b0d51f9ff22ed0740d008826f23fa749de3
sungmin <jsm5272@naver.com>
2020-02-12 10:31:47 +0900

    Updates

---
+++ precache-manifest.50237a3622ae991a9d902c31f9c2787d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0c30e07a3405afcd952918bd029318",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "2935be19154d86f8264a",
+    "url": "/movie_app_2020/static/css/main.d16425e5.chunk.css"
+  },
+  {
+    "revision": "975c1a2c1dfdbd009111",
+    "url": "/movie_app_2020/static/js/2.abca4018.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.abca4018.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2935be19154d86f8264a",
+    "url": "/movie_app_2020/static/js/main.bd754e44.chunk.js"
+  },
+  {
+    "revision": "0ee8e644ff9b4a72662b",
+    "url": "/movie_app_2020/static/js/runtime-main.5d990cee.js"
+  }
+]);
